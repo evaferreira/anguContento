276f0afeb0f43652479d5b0488380b03ba9d8b9d
Evangelina Ferreira <evaferreira92@hotmail.com>
2018-08-03 23:45:05 -0300

    Traer data del autor

--- src/app/autor/autor.component.ts
+++ src/app/autor/autor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { ContentfulService } from '../contentful.service';
+import { Entry } from 'contentful';
 
 @Component({
   selector: 'app-autor',
@@ -8,14 +10,26 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 })
 export class AutorComponent implements OnInit {
 
+  public autor: Entry<any>;
+
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private contentfulService: ContentfulService
   ) { }
 
   ngOnInit() {
-    const autor = this.route.snapshot.paramMap.get('id');
-    console.log("El autor es", autor)
+    const autorId = this.route.snapshot.paramMap.get('id');
+    console.log("El autor es", autorId);
+    
+    this.contentfulService.getAutor(autorId)
+    .then((autor) => {
+      this.autor = autor;
+    });
+  }
+  
+  goToHome() {
+    this.router.navigate(['/']);
   }
 
 }
